659e0d74f95c900cafaa3b5dad7dbdcf9d3c2485
estellelee <122672497+estellelee@users.noreply.github.com>
2023-02-07 19:39:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 $(document).ready(function () {
-  renderCurrentTime();
-  renderQuote();
-  renderRandomImage();
-});
+            renderCurrentTime();
+            renderQuote();
+        });
 
-//현재 시간
+
 function renderCurrentTime() {
-  let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      let datetime = data["datetime"].substr(11, 5);
-      // let datetime = data['datetime']
-      // console.log(datetime)
-      $("#time").text(datetime);
-    });
+    let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`;
+    fetch(url)
+        .then(res => res.json()).then((data) => {
+            let datetime = data['datetime'].substr(11,5);
+            $('#time').text(datetime);
+        });
 }
 
-// 명언
-function renderQuote() {
-  let url = `https://api.quotable.io/random`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      let content = `" ${data["content"]} "`;
-      let author = `- ${data["author"]} -`;
-      $("#content").text(content);
-      $("#author").text(author);
-    });
-}
+
+        function renderQuote() {
+            let url = `https://api.quotable.io/random`;
+            fetch(url)
+                .then(res => res.json()).then((data) => {
+                    let content = `" ${data['content']} "`;
+                    let author = `- ${data['author']} -`;
+                    $('#content').text(content);
+                    $('#author').text(author);
+                });
+        }
 
 // 랜덤 사진 꾸미기
 function renderRandomImage() {
